21fb278e3f112451163cab43175e9a63c56df6d4
Antoine Pultier <antoine.pultier@sintef.no>
2014-05-07 10:18:01 +0200

    Repository files

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt);
+	require('time-grunt')(grunt);
+
+	grunt.initConfig({
+		clean: {
+			ts: {
+				files: [{
+					src:["dist/*.js", "dist/*.d.ts", "dist/*.map"]
+				}]
+			}
+		},
+		ts: {
+			build: {
+				src:["PruneCluster.ts", "LeafletAdapter.ts"],
+				reference: "dist/PruneCluster.d.ts",
+				out:'./dist/PruneCluster.js',
+				// outDir:'build',
+				options:{
+					target: 'es5',
+					module: 'commonjs',
+					sourceMap:true
+				}
+			}
+		},
+		uglify: {
+			ts: {
+				options: {
+					sourceMap: true,
+					sourceMapName: 'dist/PruneCluster.min.js.map'
+				},
+				files: {
+					'dist/PruneCluster.min.js' : ['dist/PruneCluster.js']
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('build', [
+		'clean:ts',
+		'ts:build',
+		'uglify'
+	]);
+
+	grunt.registerTask('default', ['build']);
+}
